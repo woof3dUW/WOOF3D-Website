f3f184fe4f3974bd242f58ba686a9e78216086fd
shadowman11 <vashmav@outlook.com>
2024-06-17 21:23:35 -0700

    Fixed all problems with header and added a hover effect for the pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,13 @@ export const Header = () => {
     return (
         <>
         <header className="Header">
-            <Link to="/"><div className="HomeButton"><img className="HeaderImg" src="woof3dlogo.png" alt="WOOF3D Logo"/><h3 className="HeaderText">WOOF3D</h3></div></Link>
+            <Link className="HeaderText" to="/"><div className="HomeButton"><img className="HeaderImg" src="woof3dlogo.png" alt="WOOF3D Logo"/>WOOF3D</div></Link>
             <div className="Navbar">
                 <Link className="NavItem" to="/projects">Projects</Link>
                 <Link className="NavItem" to="/officers">Officers</Link>
                 <Link className="NavItem" to="/officehours">Office Hours</Link>
                 <Link className="NavItem" to="/resources">Member Resources</Link>
             </div>
-            <hr></hr>
         </header>
         </>
     )
