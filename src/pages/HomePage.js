2877662797d91120905e161bf5e130a5c30445c4
shadowman11 <vashmav@outlook.com>
2024-06-25 00:31:58 -0700

    Added CSS to the home page along with more content.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,32 +7,46 @@ export const HomePage = () => {
         <div className="Container">
             <Header/>
             <main className="MainContent">
-                
-                <h1>Welcome to WOOF3D</h1>
-                <h4>The 3D Printing Club at the University of Washington</h4>
-                <br></br>
-                <br></br>
-                <br></br>
-                <h1>About the Club</h1>
-                <p>As the 3D printing club at UW, we provide a hub for anyone interested in 3D printing. 
-                    Whether you’re a beginner or an expert, we have something for everyone. Experience is not required, and neither is any time commitment.
-                </p>
-                <br></br>
-                <h3>What Does the Club Do?</h3>
-                <p>Fall Quarter: We hold weekly workshops mostly aimed towards beginners. This includes everything from how to use a 
-                    3D printer to how to 3D print textiles. Show up to as many as you want!
-                </p>
-                <br></br>
-                <p>Winter and Spring Quarters: During these quarters, we provide members with the opportunity to work on a larger 
-                    scale 3D printing project with other members. Joining a team is not required but it is a good way to get hands on 
-                    experience with 3D printing and maybe even learn a bit about hardware or software in the process. 
-                    Check out the Projects tab for information on current and past projects!
-                </p>
-                <br></br>
-                <h3>Cost</h3>
-                <p>Attending all workshops is completely free. There is a $25 fee to become a full fledged club member. 
-                    This gives you access to unlimited filament, access to the club's printers, as well as free pizza at the end of each quarter at the club social.
-                </p>
+                <div className="Banner">
+                    <h1 className="BannerTextTop">Welcome to WOOF3D</h1>
+                    <h1 className="BannerTextBottom">The 3D Printing Club at the University of Washington</h1>
+                </div>
+                <div className="Section">
+                    <h1 className="Head">About the Club</h1>
+                    <p className="Text">As the 3D printing club at UW, we provide a hub for anyone interested in 3D printing. 
+                    Whether you're a beginner or an expert, we have something for everyone. Experience is not required, and neither is any time commitment.
+                    </p>
+                </div>
+                <div className="Section">
+                    <h1 className="Head">What Does the Club Do?</h1>
+                    <p className="Text">During Fall Quarter, we hold weekly workshops mostly aimed towards beginners. This includes everything from how to use a 
+                        3D printer to how to 3D print textiles. Show up to as many as you want!
+                    </p>
+                    <p className="Text">During Winter and Spring Quarters we provide members with the opportunity to work on a larger 
+                        scale 3D printing project with other members. Joining a team is not required, but it is a good way to get hands on 
+                        experience with 3D printing and learn about hardware and software in the process. 
+                        Check out the Projects tab for information on current and past projects!
+                    </p>
+                </div>
+                <div className="Section">
+                    <h1 className="Head">Cost</h1>
+                    <p className="Text">Attending all workshops is completely free. There is a $25 fee to become a full fledged club member. 
+                        This gives you access to unlimited filament, access to the club's printers, as well as free pizza at the end of each quarter at the club social.
+                    </p>
+                </div>
+                <div className="Section">
+                    <h1 className="Head">How to Join</h1>
+                    <ol className="Text">
+                        <li className="Step">Join the Discord Server</li>
+                        <div className="StepDetails">This is where all communication and announcements take place. Click <a href="https://discord.gg/neKV9u38PV" className="Link" target="_blank" rel="noreferrer noopener">here</a> to join.</div>
+                        <li className="Step">Fill out the Interest Form</li>
+                        <div className="StepDetails">This helps us keep track of the club's scale. Click <a href="https://forms.gle/XueaaQsFAmZmDSgZ8" className="Link" target="_blank" rel="noreferrer noopener">here</a> to fill out the form.</div>
+                        <li className="Step">Fill out the Fee Form (Optional)</li>
+                        <div className="StepDetails">This puts your name down for paying the club fee, which gives you access to unlimited filament, access to the 
+                            club printers and free pizza every quarter. Fees can be paid either through PayPal or by givign cash to an officer. Click <a href="https://discord.gg/neKV9u38PV" className="Link" target="_blank" rel="noreferrer noopener">here</a> to fill out the form.
+                        </div>
+                    </ol>
+                </div>
             </main>
             <Footer/>
         </div>
