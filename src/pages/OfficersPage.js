74c551495897159e4f65cad1344ffca92a07e3fe
shadowman11 <vashmav@outlook.com>
2024-09-08 23:49:13 -0700

    Made the lists of items on the officers and projects page draw from lists rather than having to be placed manually in rows like before.

--- src/pages/OfficersPage.js
+++ src/pages/OfficersPage.js
@@ -6,27 +6,29 @@ import vash from "./images/vash.jpg"
 import joshua from "./images/joshua.jpg"
 
 export const OfficersPage = () => {
+    const officers = [
+        {name: "Vash Mavrinac", role: "Software Technical Lead", picture: vash,
+        bio: `Hello, my name is Vash and I'm a second year majoring in Computer Science. Last year, I worked on the Illumination project. 
+        I especially enjoy the artistic aspect of 3D printing, such as making figures or swords. I hope to see you at the club!`},
+        {name: "Joshua Levin", role: "Education Co-Lead", picture: joshua,
+        bio: `Hi, I'm Joshua Levin. I'm a sophomore studying electrical and computer engineering. Come join the club!`}
+    ]
+    
     return (
         <div className="Container">
             <Header/>
             <main className="MainContent">
                 <h1 className="Head Center">Meet Our Officers</h1>
-                <div className="Row">
-                    <div className="RowSection">
-                        <img className="Portrait" src={vash} alt="Vash Portrait"/>
-                        <div className="Name">Vash Mavrinac</div>
-                        <div className="Role">Software Technical Lead</div>
-                        <p className="OfficerText">Hello, my name is Vash and I'm a second year majoring in Computer Science. Last year, I worked on the Illumination project. 
-                            I especially enjoy the artistic aspect of 3D printing, such as making figures or swords. I hope to see you at the club!
-                        </p>
+                {officers.map((officer, officerIndex) => (
+                    <div className="Row">
+                        <div className="RowSection" style={officerIndex % 2 === 0 ? {marginLeft: "auto"} : {marginRight: "auto"}}>
+                            <img className="Portrait" src={officer.picture} alt={officer.name}/>
+                            <div className="Name">{officer.name}</div>
+                            <div className="Role">{officer.role}</div>
+                            <p className="OfficerText">{officer.bio}</p>
+                        </div>
                     </div>
-                    <div className="RowSection">
-                        <img className="Portrait" src={joshua} alt="Joshua Portrait"/>
-                        <div className="Name">Joshua Levin</div>
-                        <div className="Role">Education Co-Lead</div>
-                        <p className="OfficerText">Hi, I'm Joshua Levin. I'm a sophomore studying electrical and computer engineering. Come join the club!</p>
-                    </div>
-                </div>
+                ))}
             </main>
             <Footer/>
         </div>
